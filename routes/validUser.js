d4f2fdec2a7d120c4b59f8b0f3a8dea1bddd70d2
BHARATH KUMAR <bhavanisandhiya58@gmail.com>
2020-09-20 03:20:01 +0530

    dashboard

--- routes/validUser.js
+++ routes/validUser.js
@@ -7,31 +7,42 @@ const jwt = require('jsonwebtoken');
 
 router.post("/", async function (req, res, next) {
     let client;
-    
+
     try {
         let token = req.body.token;
-        if(token){
+        if (token) {
             let user = jwt.verify(token, 'secret key')
             client = await mongoClient.connect(url);
             let db = client.db("zenClass");
-            let isTokenValid = await db.collection('url-users').findOne({email : user.id});
-            console.log(user.id);
-            if(isTokenValid.activated){
+            let isTokenValid = await db.collection('url-users').findOne({
+                email: user.id
+            });
+
+            if (isTokenValid.activated) {
+                if (!req.body.all) {
+                    res.json({
+                        status: 200,
+                        data: isTokenValid,
+                        messsage: "Valid User"
+                    })
+                }
+                else{
+                    let resultUrl = await db.collection('urls').find({shortURLPath : {$in : isTokenValid.urls}}).toArray();
+                    res.json({
+                        status: 200,
+                        data: resultUrl,
+                    })
+                }
+            } else {
                 res.json({
-                    status : 200,
-                    messsage : "Valid User"
-                })
-            }
-            else{
-                res.json({
-                    status : 404,
-                    messsage : "Unable to access"
+                    status: 404,
+                    messsage: "Unable to access"
                 })
             }
         }
     } catch (error) {
         res.json({
-            messsage : "no token availabe"
+            messsage: "no token availabe"
         })
     }
 
